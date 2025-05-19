5c5a6a4f675bc280d5e627783a9cc4e5f83b41d4
moha3052 <moha3052@hotmail.dk>
2025-05-19 19:31:48 +0200

    Nu kan man style i nativewind og dette er tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",    // 👈 tilføj hvis du bruger app-mappe
+    "./screens/**/*.{js,jsx,ts,tsx}", ], // 👈 hvis du bruger screens],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
